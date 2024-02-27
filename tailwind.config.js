e331f38b3dfc7459cc508970f7a09934e3ea2e72
isaac finkelstein <isaacfink123@gmail.com>
2024-02-27 16:59:54 +0000

    chore: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{ts,tsx,jsx,js,svelte,html}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
